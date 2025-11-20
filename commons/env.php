f382e03f8a249d8a9893f99553cfe46e51da59a9
HaiLongVirgo <hailongvirgo@gmail.com>
2025-11-20 22:53:30 +0700

    Merge branch 'main' of https://github.com/hoangthang183204/pro1014

--- commons/env.php
+++ commons/env.php
@@ -3,7 +3,7 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/duanmau/mvc-oop-basic/');
+define('BASE_URL'       , 'http://localhost/pro1014/');
 
 define('BASE_URL_ADMIN'       , 'http://localhost/pro1014/admin/');
 define('BASE_URL_GUIDE'       , 'http://localhost/pro1014/tour_guide/');
