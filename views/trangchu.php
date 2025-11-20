0a1e143fb2f306d34529f33aa7b486f390aadd51
panhk6dzno1 <phuanhk9@gmail.com>
2025-11-20 19:28:35 +0700

    Add files via upload

--- views/trangchu.php
+++ views/trangchu.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo $title; ?></h1>
-    <h2><?php echo $thoiTiet; ?></h2>
-</body>
-</html>
+<?php 
+// Hiển thị dashboard giống admin nhưng dùng layout hướng dẫn viên
+// Partial dashboard (admin) được reuse
+// if (session_status() !== PHP_SESSION_ACTIVE) {
+//     session_start();
+// }
+
+// Nếu chưa login -> chuyển tới trang login admin (hoặc bạn có thể đổi sang login hướng dẫn viên)
+// if (empty($_SESSION['admin_id'])) {
+//     header('Location: ' . BASE_URL_ADMIN . '?act=login');
+//     exit();
+// }
+
+// Include header/sidebar của tour_guide rồi include partial admin dashboard
+require './views/layout/header.php';
+require './views/layout/sidebar.php';
+require './views/layout/footer.php';
+?>
